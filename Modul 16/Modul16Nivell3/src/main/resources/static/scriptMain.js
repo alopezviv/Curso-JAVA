b49c032e98c72a9b463542e484b7ebf87c4ef509
alopezviv <peopole@gmail.com>
2021-10-05 19:13:15 +0200

    modul16nivell3 continuat

--- Modul 16/Modul16Nivell3/src/main/resources/static/scriptMain.js
+++ Modul 16/Modul16Nivell3/src/main/resources/static/scriptMain.js
@@ -4,7 +4,15 @@ $(document).ready(()=>{
         e.preventDefault();
         postPlayer();
     });
-
+    winner1();
+    winner2();
+    winner3();
+    losser1();
+    losser2();
+    losser3();
+    globalRanking();
+    globalRanking2();
+    globalRanking3();
 });
 
 
@@ -19,4 +27,102 @@ function postPlayer(){
         }
     });  
 }
-function juego1(){}
+function winner1(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/ranking/winner",
+        
+    }).then(function(data){
+        console.log(data.nameCheckAnonim)
+        $("#winner1").text(data.nameCheckAnonim);
+    })
+}
+function winner2(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/ranking/winner2",
+        
+    }).then(function(data){
+        console.log(data.nameCheckAnonim)
+        $("#winner2").text(data.nameCheckAnonim);
+    })
+}
+function winner3(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/ranking/winner",
+        
+    }).then(function(data){
+        console.log(data.nameCheckAnonim)
+        $("#winner3").text(data.nameCheckAnonim);
+    })
+}
+function losser1(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/ranking/losser",
+        
+    }).then(function(data){
+        console.log(data.nameCheckAnonim)
+        $("#losser1").text(data.nameCheckAnonim);
+    })
+}
+function losser2(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/ranking/losser2",
+        
+    }).then(function(data){
+        console.log(data.nameCheckAnonim)
+        $("#losser2").text(data.nameCheckAnonim);
+    })
+}
+function losser3(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/ranking/losser3",
+        
+    }).then(function(data){
+        console.log(data.nameCheckAnonim)
+        $("#losser3").text(data.nameCheckAnonim);
+    })
+}
+function globalRanking(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/ranking",
+        
+    }).then(function(data){
+        console.log(data)
+        $("#globalRanking1").text(data + "%");
+    })
+}
+function globalRanking2(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/ranking2",
+        
+    }).then(function(data){
+        console.log(data)
+        $("#globalRanking2").text(data + "%");
+    })
+}
+function globalRanking3(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/ranking3",
+        
+    }).then(function(data){
+        console.log(data)
+        $("#globalRanking3").text(data + "%");
+    })
+}
---
+++ Modul 16/Modul16Nivell3/src/main/resources/static/scriptPlayer.js
@@ -0,0 +1,131 @@
+$(document).ready(()=>{
+    console.log("JQUERY Activado")
+    getTiradasJuego1();
+    getTiradasJuego2();
+    getTiradasJuego3();
+
+});
+function juego1(){
+    let id = $("#playerId").text();
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/players/"+id+"/games",
+        success: function (response) {
+            console.log("Game 1 played");
+        }
+    });
+    location.reload();
+}
+function juego2(){
+    let id = $("#playerId").text();
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/players/"+id+"/games2",
+        success: function (response) {
+            console.log("Game 2 played");
+        }
+    });
+    location.reload();
+}
+function juego3(){
+    let id = $("#playerId").text();
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/players/"+id+"/games3",
+        success: function (response) {
+            console.log("Game 3 played");
+        }
+    });
+    location.reload();
+}
+function getTiradasJuego1(){
+    let id = $("#playerId").text();
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/"+id+"/games",
+        
+    }).then(function(data){
+        console.log(data)
+        $.each(data, (index) =>{
+            $("#tiradasJuego1").append("<tr>");
+            $("#tiradasJuego1").append("<td>" + data[index].dice1 + "</td>");
+            $("#tiradasJuego1").append("<td>" + data[index].dice2 + "</td>");
+            if(data[index].dice1+data[index].dice2 == 7){
+                $("#tiradasJuego1").append("<td>Si</td>");
+            }else{
+                $("#tiradasJuego1").append("<td>No</td>");
+            }
+            $("#tiradasJuego1").append("</tr>");
+        })
+    })
+}
+function getTiradasJuego2(){
+    let id = $("#playerId").text();
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/"+id+"/games2",
+        
+    }).then(function(data){
+        console.log(data)
+        $.each(data, (index) =>{
+            $("#tiradasJuego2").append("<tr>");
+            $("#tiradasJuego2").append("<td>" + data[index].dice1 + "</td>");
+            $("#tiradasJuego2").append("<td>" + data[index].dice2 + "</td>");
+            if(data[index].dice1+data[index].dice2 == 8){
+                $("#tiradasJuego2").append("<td>Si</td>");
+            }else{
+                $("#tiradasJuego2").append("<td>No</td>");
+            }
+            $("#tiradasJuego2").append("</tr>");
+
+        })
+    })
+}
+function getTiradasJuego3(){
+    let id = $("#playerId").text();
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/players/"+id+"/games3",
+        
+    }).then(function(data){
+        console.log(data)
+        $.each(data, (index) =>{
+            $("#tiradasJuego3").append("<tr>");
+            $("#tiradasJuego3").append("<td>" + data[index].dice1 + "</td>");
+            $("#tiradasJuego3").append("<td>" + data[index].dice2 + "</td>");
+            if(data[index].dice1+data[index].dice2 > 7){
+                $("#tiradasJuego3").append("<td>Si</td>");
+            }else{
+                $("#tiradasJuego3").append("<td>No</td>");
+            }
+            $("#tiradasJuego3").append("</tr>");
+        })
+    })
+}
+function borrarTiradas1(){
+    let id = $("#playerId").text();
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:8080/players/"+id+"/games",
+        
+    })
+    location.reload();
+}
+function borrarTiradas2(){
+    let id = $("#playerId").text();
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:8080/players/"+id+"/games2",
+        
+    })
+    location.reload();
+}
+function borrarTiradas3(){
+    let id = $("#playerId").text();
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:8080/players/"+id+"/games3",
+        
+    })
+    location.reload();
+}
